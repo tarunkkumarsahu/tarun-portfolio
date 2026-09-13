17055d32fbc17b6fc0d1286be9c23fc342eb9ca6
Tarun Kumar Sahu <tarunkumarsahu354@gmail.com>
2026-09-13 15:57:06 +0530

    Load integrated hero overrides last

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ import "./lab-photo.css";
 import "./lab-page-final.css";
 import "./hero-3d.css";
 import "./platform.css";
+import "./hero-integrated.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
